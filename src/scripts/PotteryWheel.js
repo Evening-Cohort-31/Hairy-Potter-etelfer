25c62f73a08e07bfa85f8c633c1c079fe6d2d1cd
Erin Telfer <eringtelfer@gmail.com>
2025-08-09 12:50:07 -0500

    created makePottery function and used to create 5 pottery pieces, each with name, weight, height, and incrementing id

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,14 @@
+let potteryId = 1
+
+export const makePottery = (potteryShape, potteryWeight, potteryHeight) => {
+
+let potteryObject = {
+    shape: potteryShape,
+    weight: potteryWeight,
+    height: potteryHeight,
+    id: potteryId 
+
+}
+potteryId++
+return potteryObject
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,19 @@
 // Imports go first
 
-
+import { makePottery } from "./PotteryWheel.js"
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("Mug", 2, 7)
+let platter = makePottery("Platter", 6, 1)
+let bowl = makePottery("Bowl", 3, 4);
+let vase = makePottery("Vase", 2, 8);
+let cup = makePottery("Cup", 1, 3);
+
+console.log(mug)
+console.log(platter)
+console.log(bowl)
+console.log(vase)
+console.log(cup)
 
 
 // Fire each piece of pottery in the kiln
